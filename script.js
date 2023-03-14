9fd704a01368801929bcb09ed40132a9c5869665
Sophie <totalsophie@gmail.com>
2023-03-14 15:06:53 +0300

    Add some changes to the script file

--- script.js
+++ script.js
@@ -22,6 +22,6 @@ checkValidity(birthday)
 // Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
 
 function getDayOfWeek(calendarDate){
-    
+    // Comment added
 
 }
